90ca1de56143839b4d0ec6efaf5ed7b48bce88a3
Tony Cen Cen <97487791+EPmaple@users.noreply.github.com>
2024-03-01 19:11:35 -0500

    Update Constants.java

--- src/Constants.java
+++ src/Constants.java
@@ -11,6 +11,23 @@ public final class Constants {
     public static final int TEAM_MAX_SIZE = 5;
     public static final int TEAM_MIN_SIZE = 1;
 
+    // Tile coordinates
+    public static final int MIN_X = 0;
+    public static final int MIN_Y = 0;
+
+    // Tile edges
+    public static final int NORTHEDGE = 0;
+    public static final int EASTEDGE = 1;
+    public static final int SOUTHEDGE = 2;
+    public static final int WESTEDGE = 3;
+
+    // Pieces
+    public static final int COLORPIECE = 4;
+    public static final int TEAMPIECE = 5;
+
     // For Quit
     public static final int QUIT_VALUE = -1;
+
+    // For invalid selections
+    public static final int INVALID_OPT = -99;
 }
